8b4fd0dd31b7db767e14c68d7e0fb162d78e5dc1
denis <9172885049@mail.ru>
2022-09-05 21:37:21 +0300

    bug fix

--- PhotoSearchProject/Services/Models/SearchResults.swift
+++ PhotoSearchProject/Services/Models/SearchResults.swift
@@ -11,24 +11,3 @@ struct SearchResults: Decodable {
     let total: Int
     let results: [Photo]
 }
-
-struct Photo: Decodable {
-    let id: String
-    let width: Int
-    let height: Int
-    let created_at: String
-    let urls: [URLKing.RawValue: String]
-    let user: User
-
-    enum URLKing: String {
-        case raw
-        case full
-        case regular
-        case small
-        case thumb
-    }
-}
-
-struct User: Decodable {
-    let name: String
-}
